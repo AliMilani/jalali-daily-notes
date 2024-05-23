8b69c4d41477eabb5d705cebdb2b4d5dbd9a705a
Ali Milani <amilani.ir@gmail.com>
2024-05-23 14:17:07 +0330

    feat: remove useless options

--- main.ts
+++ main.ts
@@ -14,102 +14,55 @@ import moment from "moment-jalaali";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface PluginSettings {
 	dailyNotePath: string;
 	templateFilePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: PluginSettings = {
 	dailyNotePath: "",
 	templateFilePath: "",
 };
-
+const createNewDailyNote = async (plugin: MyPlugin) => {
+	const todayJalali = moment().format("jYYYY-jMM-jDD");
+	const targetPath = `${plugin.settings.dailyNotePath}/${todayJalali}.md`;
+	if (plugin.app.vault.getAbstractFileByPath(targetPath)) {
+		new Notice("File already exists");
+		return;
+	}
+	// plugin.app.vault.rename(file, targetPath);
+	await plugin.app.vault.adapter.copy(
+		plugin.settings.templateFilePath,
+		targetPath
+	);
+	// open the file
+	plugin.app.workspace.openLinkText(targetPath, "", true);
+	return;
+};
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
+		this.addRibbonIcon(
 			"calendar-heart",
 			"تبدیل تاریخ فعلی به تاریخ شمسی",
 			async (evt: MouseEvent) => {
 				// Called when the user clicks the icon.
-				const todayJalali = moment().format("jYYYY-jMM-jDD");
-				const targetPath = `${this.settings.dailyNotePath}/${todayJalali}.md`;
-				if (this.app.vault.getAbstractFileByPath(targetPath)) {
-					new Notice("File already exists");
-					return;
-				}
-				// this.app.vault.rename(file, targetPath);
-				await this.app.vault.adapter.copy(
-					this.settings.templateFilePath,
-					targetPath
-				);
-				// open the file
-				this.app.workspace.openLinkText(targetPath, "", true);
-				return;
+				await createNewDailyNote(this);
 			}
 		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
+			id: "create-new-jalali-daily-note",
+			name: "Create new jalali daily note",
 			callback: () => {
-				new SampleModal(this.app).open();
-			},
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				createNewDailyNote(this);
 			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new defaultSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {}
@@ -124,22 +77,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
 	}
 }
 
